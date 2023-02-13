fcd7f0bd19b3e0cf673e63687f1c16857f16ed4c
Apollo Wayne <waynezhaoyang@gmail.com>
2023-02-13 20:38:53 +0800

    Add Solutions

---
+++ solutions/09155-hard-validdate.ts
@@ -0,0 +1,30 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<ValidDate<'0102'>, true>>,
+  Expect<Equal<ValidDate<'0131'>, true>>,
+  Expect<Equal<ValidDate<'1231'>, true>>,
+  Expect<Equal<ValidDate<'0229'>, false>>,
+  Expect<Equal<ValidDate<'0100'>, false>>,
+  Expect<Equal<ValidDate<'0132'>, false>>,
+  Expect<Equal<ValidDate<'1301'>, false>>,
+  Expect<Equal<ValidDate<'0123'>, true>>,
+  Expect<Equal<ValidDate<'01234'>, false>>,
+  Expect<Equal<ValidDate<''>, false>>,
+]
+
+
+// ============= Your Code Here =============
+type ValidDate<T extends string> = T extends `${infer A extends string}${infer B extends string}${infer C extends string}${infer D extends string}` ?
+  `${A}${B}` extends '01' | '03' | '05' | '07' | '08' | '10' | '12' ?
+    `${C}${D}` extends '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' ?
+    true : false :
+  `${A}${B}` extends '04' | '06' | '09' | '11' ?
+    `${C}${D}` extends '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' ?
+    true : false :
+  `${A}${B}` extends '02' ?
+    `${C}${D}` extends '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' ?
+    true : false :
+    false : false
+  
